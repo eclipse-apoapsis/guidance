5fb914741144e70638c04b28c26c2de6bf3c248c
Marcel Kurzmann <marcel.kurzmann@bosch.com>
2024-08-15 15:41:46 +0200

    website built with docusaurus in build-folder

---
+++ website/build/assets/js/f8551aab.e9915f92.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkeclipse_apoapsis=self.webpackChunkeclipse_apoapsis||[]).push([[2224],{1976:(e,s,a)=>{a.r(s),a.d(s,{default:()=>i});a(6540);var p=a(781),c=a(4848);function i(){return(0,c.jsxs)(p.A,{children:[(0,c.jsx)("h1",{children:"My React page"}),(0,c.jsx)("p",{children:"This is a React page"})]})}}}]);
